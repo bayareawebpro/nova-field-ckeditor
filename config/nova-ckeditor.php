daf02fd78585fd5fcff1d94d607f59de58a228c8
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-29 05:13:27 -0700

    Update nova-ckeditor.php

--- config/nova-ckeditor.php
+++ config/nova-ckeditor.php
@@ -1,8 +1,20 @@
 <?php
 return [
+    /**
+     * Intervention Image Max Dimensions
+     * @docs http://image.intervention.io/
+     */
     'max_width'=>1024,
     'max_height'=>768,
 
+    /**
+     * Intervention Image Max Memory (php.ini override)
+     */
     'memory'=>'256M',
+
+    /**
+     * Spatie Image Optimizer Bin Path
+     * @docs https://github.com/spatie/image-optimizer
+     */
     'bin_path'=>'/usr/local/bin',
 ];
