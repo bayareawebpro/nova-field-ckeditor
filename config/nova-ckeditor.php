7d597119710607a873dbac165e2e1927e58a476c
Dan Alvidrez <dan@bayareawebpro.com>
2020-04-29 04:44:38 -0700

    Update nova-ckeditor.php

--- config/nova-ckeditor.php
+++ config/nova-ckeditor.php
@@ -2,9 +2,7 @@
 return [
     'max_width'=>1024,
     'max_height'=>768,
-    'bin_path'=>'/usr/local/bin',
+
     'memory'=>'256M',
-    'meta_title' => 'Site Title',
-    'meta_description' => "Site Description",
-    'meta_image' => "/assets/og-logo.png",
+    'bin_path'=>'/usr/local/bin',
 ];
